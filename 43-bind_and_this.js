41ef7a52ae739d114a8d10ff8631928c5a2129de
Dahyro <Dahyro>
2016-12-29 12:43:09 -0400

    first commit

---
+++ 43-bind_and_this.js
@@ -0,0 +1,11 @@
+let dog = {
+    sound: 'woof',
+    talk: function(){
+        console.log(this.sound)
+    }
+}
+
+dog.talk();
+let talkFunction  = dog.talk;
+let boundFunction = talkFunction.bind(dog);
+boundFunction();
